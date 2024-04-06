5724ec35b9d6b9ea1ecd0ff835797428e68819b9
mi-nadir <vipok72@yandex.ru>
2024-04-06 12:04:16 +0300

    if else

---
+++ src/Temp.java
@@ -0,0 +1,12 @@
+public class Temp {
+    public static void main(String[] args) {
+        int temp = 23;
+        if(temp > 25) {
+            System.out.println("Кондиционер включен");
+        } else if(temp < 22) {
+            System.out.println("Кондицинер включен");
+        } else {
+            System.out.println("Кондиционер ничего не делает");
+        }
+    }
+}
